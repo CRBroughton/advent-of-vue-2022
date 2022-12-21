780b09e833a3e6c10aeb71cfb422d92918acca3d
CRBroughton <crbroughton@posteo.uk>
2022-12-21 15:48:08 +0000

    feat: :sparkles: completed day five of Advent of Vue

--- day-V/src/App.vue
+++ day-V/src/App.vue
@@ -1,20 +1,37 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const to = ref('')
+const from = ref('')
+</script>
+
 <template>
   <div class="w-full h-full flex justify-center items-center">
     <div>
       <div class="w-full max-w-2xl p-4 rounded bg-red-400 text-white">
         <div class="border-2 rounded flex flex-col md:flex-row md:items-center md:gap-8 p-4">
-          <img src="/assets/santa.svg" alt="" class="max-w-[12em]" />
+          <img src="/assets/santa.svg" alt="" class="max-w-[12em]">
           <div>
-            <h1 class="font-serif text-4xl">Happy Holidays!</h1>
-            <div class="mt-4 bg-white text-black p-2 rounded">TO: John</div>
-            <div class="mt-2 bg-white text-black p-2 rounded">FROM: Joana</div>
+            <h1 class="font-serif text-4xl">
+              Happy Holidays!
+            </h1>
+            <div class="mt-4 bg-white text-black p-2 rounded">
+              TO: {{ to }}
+            </div>
+            <div class="mt-2 bg-white text-black p-2 rounded">
+              FROM: {{ from }}
+            </div>
           </div>
         </div>
+        <label>
+          To:
+          <input v-model="to" type="text">
+        </label>
+        <label>
+          From:
+          <input v-model="from" type="text">
+        </label>
       </div>
     </div>
   </div>
 </template>
-
-<script setup>
-// ...
-</script>
