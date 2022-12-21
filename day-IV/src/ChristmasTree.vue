af620fd207e4d26aaf173421145aa0683a720991
CRBroughton <crbroughton@posteo.uk>
2022-12-21 15:31:18 +0000

    feat: :sparkles: completed day four of Advent of Vue

---
+++ day-IV/src/ChristmasTree.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    size: number
+  }>(),
+  {
+    size: 1,
+  },
+)
+</script>
+
+<template>
+  <div>
+    <ChristmasTree v-if="size > 1" :size="size - 1" />
+    <div class="flex flex-row justify-center">
+      <!-- This div is styled for the tree sections, but it's not complete -->
+      <div v-for="bauble in size" :key="bauble" class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
+    </div>
+  </div>
+</template>
